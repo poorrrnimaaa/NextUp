8af0dd172e94e4f618e54c013d1cc0db2370160c
Poornima <152963215+poorrrnimaaa@users.noreply.github.com>
2026-06-10 21:32:58 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -46,3 +46,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("NextUp Backend Running");
+});
